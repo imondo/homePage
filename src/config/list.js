d02329cc011d5c361d10abb5672b1e2e92509b2c
WIN-QBII8O4JJKB\Mondo <568197478@qq.com>
2022-11-16 17:13:51 +0800

    feat: 修改主页

--- src/config/list.js
+++ src/config/list.js
@@ -1,8 +1,13 @@
 export default [
     {
-        url: '/query_car',
+        url: 'http://imondo.cn/files/apps/car',
         title: '查车牌',
-        rich: '查询车牌归属地和车辆品牌的应用。'
+        rich: '查询车牌归属地、汽车品牌、今日油价。'
+    },
+    {
+        url: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/e074e4c045654722a6f3a36c81fe3231~tplv-k3u1fbpfcp-zoom-in-crop-mark:3024:0:0:0.awebp?',
+        title: '地址备忘录Lite',
+        rich: '清爽的地址管理微信小程序。'
     },
     {
         url: 'http://news.imondo.cn/',
